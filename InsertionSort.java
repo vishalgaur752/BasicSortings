9a10b7636fbd172df279f246dccdf964b63ce04f
vishalgaur752 <vishalgaur752@gmail.com>
2024-05-08 12:47:23 +0530

    Basic Sorting Files add

---
+++ InsertionSort.java
@@ -0,0 +1,28 @@
+package BasicSortings;
+
+public class InsertionSort {
+    public static void insertSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] < curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 6, 2, 1, 8, 7, 4, 5, 3, 1 };
+        insertSort(arr);
+        printArr(arr);
+    }
+}
---
+++ SelectionSort.java
@@ -0,0 +1,30 @@
+package BasicSortings;
+
+public class SelectionSort {
+    public static void selctionsort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j])
+                    ;
+                minPos = arr[j];
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 6, 2, 1, 8, 7, 4, 5, 3, 1 };
+        selctionsort(arr);
+        printArr(arr);
+    }
+}
